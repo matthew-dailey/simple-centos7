b82baddafdd9b676920caecde7d864be0c869ba3
Matt Dailey <matthew.dailey1@gmail.com>
2016-01-01 23:29:39 -0500

    recipe will set up authorized_keys for users

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,8 +11,10 @@ if users.empty? then
   Chef::Application::fatal!("Could not find any users in data_dag #{user_bag}", 1)
 end
 
+sudoer_names = []
 users.each do |user_data|
-  user user_data['id'] do
+  username = user_data['id']
+  user username do
     comment user_data['comment']
     uid user_data['uid']
     gid user_data['gid']
@@ -20,9 +22,28 @@ users.each do |user_data|
     shell user_data['shell']
     password user_data['password_shadow_hash']
   end
+
+  # make .ssh directory
+  directory "/home/#{username}/.ssh" do
+    owner username
+    mode '700'
+  end
+
+  # copy in public keys from public_key data_bag
+  user_key = data_bag_item('public_keys', username)
+  public_keys = user_key['public_keys'].join('\n')
+  file "/home/#{username}/.ssh/authorized_keys" do
+    owner username
+    mode '644'
+    content public_keys
+  end
+
+  if user_data['sudoer'] then
+    sudoer_names.push(username)
+  end
 end
 
-sudoer_names = users.select {|user| user['sudoer'] == true }.collect {|user| user['id']}
+# give sudoers sudo
 group 'wheel' do
   members sudoer_names
 end
