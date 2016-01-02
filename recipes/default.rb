96fbbb053fd44468bfe264931867fc1ab6cc93c2
Matt Dailey <matthew.dailey1@gmail.com>
2016-01-01 22:15:18 -0500

    Started recipe to create developer user

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# grab user information from the data bag
+user_bag = 'users'
+users = search(user_bag, "*:*")
+if users.empty? then
+  Chef::Application::fatal!("Could not find any users in data_dag #{user_bag}", 1)
+end
+
+users.each do |user_data|
+  user user_data['id'] do
+    comment user_data['comment']
+    uid user_data['uid']
+    gid user_data['gid']
+    home user_data['home']
+    shell user_data['shell']
+    password user_data['password_shadow_hash']
+  end
+end
